caae5dbab90cadc063ad139f088a398e9a4d014c
Zenith30-droid <ZobairHasan1111@gmail.com>
2024-09-26 14:51:00 +0600

    updated controller code

--- Assets/V1.0/Scripts/PlayerController.cs
+++ Assets/V1.0/Scripts/PlayerController.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] GameObject wings;
+
+    private bool isMovingForward;
+    private bool isMovingHorizontal;
 
     public void Update()
     {
@@ -30,12 +37,62 @@ public class PlayerController : MonoBehaviour
         //Vertical Y is responsible for movement in Z axis
         Vector2 rightStickAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
 
-        if (Mathf.Abs(rightStickAxis.y) >= Mathf.Abs(rightStickAxis.x) && Mathf.Abs(rightStickAxis.y) !=0)
+        if (Mathf.Abs(rightStickAxis.y) >= Mathf.Abs(rightStickAxis.x))
+        {
+            isMovingForward = true;
+            isMovingHorizontal = false;
             transform.position += transform.forward * rightStickAxis.y * 2 * Time.deltaTime;
-        else 
+        }
+        else
+        {
+            isMovingForward = false;
+            isMovingHorizontal = true;
             transform.position += transform.right * rightStickAxis.x * 2 * Time.deltaTime;
+        }
+
+        if(Mathf.Abs(rightStickAxis.y) == 0)
+            isMovingForward = false;
+
+        if (Mathf.Abs(rightStickAxis.x) == 0)
+            isMovingHorizontal = false;
 
 
-        Debug.LogError($"Moving");
+
+        PassiveRotation(
+            new Joystick(leftStickAxis),
+            new Joystick(rightStickAxis)
+            );
+
+        
+    }
+
+    public void PassiveRotation(Joystick LStick, Joystick RStick)
+    {
+        //if(RStick.yAxis != 0 && Mathf.Abs(wings.transform.eulerAngles.z) < 180f)
+        //    wings.transform.Rotate(0, 0, Time.deltaTime * 100f * -Mathf.Sign(RStick.yAxis));
+
+        //Debug.LogError($"Rotating  {Mathf.Abs(wings.transform.eulerAngles.z)}");
+
+        // Rotate the GameObject
+        if (isMovingForward)
+            wings.transform.Rotate(0, 0, Time.deltaTime * 10f * Mathf.Sign(RStick.yAxis));
+        
+        if (isMovingHorizontal)
+            wings.transform.Rotate(Time.deltaTime * 10f * Mathf.Sign(RStick.xAxis), 0, 0);
+        
+    }
+
+
+}
+public class Joystick
+{
+    public float xAxis;
+    public float yAxis;
+
+    public Joystick(Vector2 Coordinates)
+    {
+        xAxis = Coordinates.x;
+        yAxis = Coordinates.y;
     }
 }
+
